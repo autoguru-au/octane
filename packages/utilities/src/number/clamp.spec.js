adaeb2023f8439842bec6743d88d5255290dffa7
Amir Zahedi <amir@autoguru.com.au>
2019-05-22 10:14:47 +1000

    feat(utilities): Adds clamping number util function (#4)

---
+++ packages/utilities/src/number/clamp.spec.js
@@ -0,0 +1,17 @@
+import { clamp } from './clamp';
+
+describe('Utils/Clamp', () => {
+	it.each`
+		min   | max  | value | result
+		${10} | ${9} | ${11} | ${10}
+		${11} | ${9} | ${11} | ${11}
+		${9}  | ${9} | ${11} | ${9}
+		${12} | ${9} | ${11} | ${11}
+		${8}  | ${9} | ${11} | ${9}
+	`(
+		`should pass for min: $min max: $max with val: $value -> $result`,
+		({ min, max, value, result }) => {
+			expect(clamp(min, max, value)).toEqual(result);
+		}
+	);
+});
