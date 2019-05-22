eefe8d6a6db519c319f3830390a78b120a13581c
Marais Rossouw <me@maraisr.com>
2019-05-22 10:14:02 +1000

    test: Using Jest .each

--- packages/utilities/src/number/clamp.spec.js
+++ packages/utilities/src/number/clamp.spec.js
@@ -1,18 +1,17 @@
 import { clamp } from './clamp';
 
-describe('Utils/clamp', () => {
-	it('should return same value when within boundaries', () =>
-		expect(clamp(10, 9, 11)).toEqual(10));
-
-	it('should return same value when equal to upper limit', () =>
-		expect(clamp(11, 9, 11)).toEqual(11));
-
-	it('should return same value when equal to lower limit', () =>
-		expect(clamp(9, 9, 11)).toEqual(9));
-
-	it('should return max value value when greater than upper limit', () =>
-		expect(clamp(12, 9, 11)).toEqual(11));
-
-	it('should return min value value when less than lower limit', () =>
-		expect(clamp(8, 9, 11)).toEqual(9));
+describe('Utils/Clamp', () => {
+	it.each`
+		min   | max  | value | result
+		${10} | ${9} | ${11} | ${10}
+		${11} | ${9} | ${11} | ${11}
+		${9}  | ${9} | ${11} | ${9}
+		${12} | ${9} | ${11} | ${11}
+		${8}  | ${9} | ${11} | ${9}
+	`(
+		`should pass for min: $min max: $max with val: $value -> $result`,
+		({ min, max, value, result }) => {
+			expect(clamp(min, max, value)).toEqual(result);
+		}
+	);
 });
