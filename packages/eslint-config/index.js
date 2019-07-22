938c316599a88e64276880daddf554682486348a
Marais Rossouw <me@maraisr.com>
2019-07-23 09:44:43 +1000

    feat(eslint-config): Import and promise errors now

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,13 +1,21 @@
 module.exports = {
-	extends: ['xo/esnext', 'plugin:unicorn/recommended', 'prettier'],
+	extends: [
+		'xo/esnext',
+		'plugin:unicorn/recommended',
+		'kentcdodds/import',
+		'plugin:promise/recommended',
+		'prettier',
+	],
 
 	rules: {
 		'func-names': ['warn', 'always'],
+		'no-multi-assign': 'warn',
 		'no-void': 'off',
 		'no-warning-comments': ['off', { terms: ['TODO'] }],
+		'promise/always-return': 'warn',
+		'promise/catch-or-return': 'warn',
 		'unicorn/filename-case': 'off',
 		'unicorn/prevent-abbreviations': 'off',
-		'no-multi-assign': 'warn',
 		camelcase: 'off',
 	},
 };
