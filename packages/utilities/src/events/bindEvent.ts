7fba67e08fd4aee4bcc606997f52ddc735ac8bac
Marais Rossouw <me@maraisr.com>
2019-09-05 13:37:25 +1000

    fix(utilities): Type defs for bindEvent is broken

--- packages/utilities/src/events/bindEvent.ts
+++ packages/utilities/src/events/bindEvent.ts
@@ -1,14 +1,31 @@
+/* This is what the impl should be - but until TypeDefintions emit properly, we'll have to stick to any's.
+
+@see https://github.com/microsoft/TypeScript/issues/33254
+
 export const bindEvent = <
 	Node extends HTMLElement | HTMLDocument,
 	K extends keyof HTMLElementEventMap
 >(
 	node: Node,
-	event: K,
+	type: K,
 	callback: (event: HTMLElementEventMap[K]) => unknown,
 ) => {
-	node.addEventListener(event, callback as EventListener);
+	node.addEventListener(type, callback as EventListener);
 
 	return () => {
-		node.removeEventListener(event, callback as EventListener);
+		node.removeEventListener(type, callback as EventListener);
+	};
+};
+*/
+
+export const bindEvent = <Node extends HTMLElement | HTMLDocument>(
+	node: Node,
+	type: string,
+	callback: (event: any) => unknown,
+) => {
+	node.addEventListener(type, callback as EventListener);
+
+	return () => {
+		node.removeEventListener(type, callback as EventListener);
 	};
 };
