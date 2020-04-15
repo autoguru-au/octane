aa818d520c5cc57632022c65f5845a4d2aa1ef52
Marais Rossouw <me@maraisr.com>
2020-04-15 13:26:51 +1000

    chore: Bump gdu version

--- packages/gdu/commands/graphqlSchema.ts
+++ packages/gdu/commands/graphqlSchema.ts
@@ -12,21 +12,22 @@ import { CALLING_WORKSPACE_ROOT } from '../lib/roots';
 
 interface EndpointExtension extends GraphQLConfigExtension {
 	url: string;
-	method: string;
+	method: 'GET' | 'POST';
 	headers: Record<string, string>;
 }
 
 const debug = require('debug')('gdu:commands:graphqlSchema');
 
 export default async options => {
-
 	const graphQLConfig = await loadConfig({
 		rootDir: CALLING_WORKSPACE_ROOT,
 	});
 
 	const config = graphQLConfig.getDefault();
 
-	const endpointConfig = config.extension('endpoints')[options.endpoint] as EndpointExtension | undefined;
+	const endpointConfig = config.extension('endpoints')[options.endpoint] as
+		| EndpointExtension
+		| undefined;
 
 	if (typeof endpointConfig === 'undefined') {
 		throw new TypeError(`Endpoint ${options.endpoint} doesnt exist`);
@@ -45,11 +46,16 @@ export default async options => {
 	console.log(' > Schema has been checked, and is ready to use! ✅️');
 };
 
-async function loadLocalSchema(config: GraphQLProjectConfig): Promise<string | null> {
+async function loadLocalSchema(
+	config: GraphQLProjectConfig,
+): Promise<string | null> {
 	let schemaString = null;
 
 	try {
-		schemaString = readFileSync(resolve(config.dirpath, config.schema as string), 'utf8');
+		schemaString = readFileSync(
+			resolve(config.dirpath, config.schema as string),
+			'utf8',
+		);
 	} catch (error) {
 		debug(error);
 	}
@@ -57,14 +63,22 @@ async function loadLocalSchema(config: GraphQLProjectConfig): Promise<string | n
 	return schemaString;
 }
 
-async function getRemoteSchema(_config: GraphQLProjectConfig, endpointConfig: EndpointExtension): Promise<GraphQLSchema> {
-	return (await new UrlLoader().load(endpointConfig.url, {
-		method: endpointConfig.method,
-		headers: endpointConfig.headers,
-	})).schema;
+async function getRemoteSchema(
+	_config: GraphQLProjectConfig,
+	endpointConfig: EndpointExtension,
+): Promise<GraphQLSchema> {
+	return (
+		await new UrlLoader().load(endpointConfig.url, {
+			method: endpointConfig.method,
+			headers: endpointConfig.headers,
+		})
+	).schema;
 }
 
-async function updateSchemaIfHasNew(config: GraphQLProjectConfig, endpointConfig: EndpointExtension) {
+async function updateSchemaIfHasNew(
+	config: GraphQLProjectConfig,
+	endpointConfig: EndpointExtension,
+) {
 	const checksum = await getSchemaChecksum(config, endpointConfig);
 	const schemaRegex = /^#\schecksum:\s([a-z0-9]+)/;
 
@@ -73,7 +87,6 @@ async function updateSchemaIfHasNew(config: GraphQLProjectConfig, endpointConfig
 	const schemaData = await loadLocalSchema(config);
 
 	if (schemaRegex.test(schemaData)) {
-
 		const [, oldChecksum] = schemaRegex.exec(schemaData);
 
 		if (oldChecksum !== checksum) {
@@ -89,14 +102,23 @@ async function updateSchemaIfHasNew(config: GraphQLProjectConfig, endpointConfig
 	}
 }
 
-async function downloadNewSchema(config: GraphQLProjectConfig, endpointConfig: EndpointExtension) {
+async function downloadNewSchema(
+	config: GraphQLProjectConfig,
+	endpointConfig: EndpointExtension,
+) {
 	const remoteSchema = await getRemoteSchema(config, endpointConfig);
 	const checksum = await getSchemaChecksum(config, endpointConfig);
 
-	await writeFileAsync(config.schema as string, `# checksum: ${checksum}\n\n${printSchema(remoteSchema)}`);
+	await writeFileAsync(
+		config.schema as string,
+		`# checksum: ${checksum}\n\n${printSchema(remoteSchema)}`,
+	);
 }
 
-async function getSchemaChecksum(_config: GraphQLProjectConfig, endpointConfig: EndpointExtension): Promise<string> {
+async function getSchemaChecksum(
+	_config: GraphQLProjectConfig,
+	endpointConfig: EndpointExtension,
+): Promise<string> {
 	const cl = new GraphQLClient(endpointConfig.url, {
 		headers: endpointConfig.headers,
 		method: endpointConfig.method,
