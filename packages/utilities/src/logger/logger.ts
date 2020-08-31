e0b39a83e750855760611de518c3bc341841f7da
Marais Rossouw <me@maraisr.com>
2020-08-31 18:13:40 +1000

    feat(utilities): Better logger that is browser aware

--- packages/utilities/src/logger/logger.ts
+++ packages/utilities/src/logger/logger.ts
@@ -1,37 +1,60 @@
+type Level = 'debug' | 'info' | 'error';
+
+interface Payload<T> {
+	type: string;
+	level: Level;
+	message: string | Error;
+	payload: T;
+}
+
 export const createLogger = (realm: string) => {
 	return {
+		debug: createLogThing('debug', realm),
 		info: createLogThing('info', realm),
 		error: createLogThing('error', realm),
 	};
 };
 
-const createLogThing = (level: string, realm: string) => (
-	message: string,
+const createLogThing = (level: Level, realm: string) => (
+	message: string | Error,
 	extra?: object | string | number,
-) =>
+) => {
+	if (level === 'debug' && process.env.NODE_ENV === 'production') return;
+
 	printPayload({
 		type: realm,
 		level,
 		message,
 		payload: extra,
 	});
+};
 
-const printPayload = (payload) => {
-	const decorated = {
+const printPayload = <T>(payload: Payload<T>) => {
+	let decorated = {
 		...payload,
 		timestamp: new Date().toISOString(),
 	};
 
-	if (process.env.NODE_ENV === 'production') {
-		console.log(JSON.stringify(decorated));
+	if (decorated.message instanceof Error) {
+		const error = decorated?.message;
+		decorated.message = error.message;
+
+		// @ts-ignore
+		if (!process.browser)
+			decorated.payload = Object.assign({}, decorated.payload, {
+				stackTrace: error?.stack,
+			});
+	}
+
+	const logMethod = getConsoleMethod(payload);
+
+	// @ts-ignore
+	if (process.browser) {
+		logMethod(`${decorated.type} :: ${decorated.message}`, decorated);
 	} else {
-		console.log(
-			`${decorated.timestamp} (${decorated.level}) | ${
-				decorated.type
-			} | ${JSON.stringify({
-				message: decorated.message,
-				payload: decorated.payload,
-			})}`,
-		);
+		logMethod(JSON.stringify(decorated));
 	}
 };
+
+export const getConsoleMethod = <T>(payload: Payload<T>) =>
+	payload.level === 'error' ? console.error : console.log;
