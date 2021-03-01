38272663d2241f2c34367a3f2bc619127efd59bf
Marais Rossouw <me@maraisr.com>
2021-03-01 14:24:26 +1000

    fix: eslint-plugin prettier changes

---
+++ .changeset/green-ants-cry.md
@@ -0,0 +1,5 @@
+---
+'@autoguru/eslint-plugin': patch
+---
+
+Prettier just has itself (no more /react)
--- packages/eslint-plugin/configs/react.js
+++ packages/eslint-plugin/configs/react.js
@@ -1,5 +1,5 @@
 module.exports = {
-	extends: ['plugin:react/recommended', 'prettier/react'],
+	extends: ['plugin:react/recommended'],
 
 	plugins: ['react', 'react-hooks', 'jsx-a11y'],
 
