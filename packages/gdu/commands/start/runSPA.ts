e8ae9fcaaff2343eb4f266ca4c8d998b970ab4eb
Sergio Gimenez <36163163+sgimama@users.noreply.github.com>
2024-06-12 10:24:16 +0800

    feat: add default value standalone in build file

--- packages/gdu/commands/start/runSPA.ts
+++ packages/gdu/commands/start/runSPA.ts
@@ -40,7 +40,7 @@ export const runSPA = async (guruConfig: GuruConfig, isDebug) => {
 
 	// eslint-disable-next-line unicorn/prefer-prototype-methods
 	const webpackConfig: Configuration = hooks.webpackConfig
-		.call(webpackConfigs(appEnv, isDebug, null, guruConfig.standalone))
+		.call(webpackConfigs(appEnv, isDebug, null, guruConfig?.standalone))
 		.find(({ name }) => name === appEnv);
 
 	const consumerHtmlTemplate = getConsumerHtmlTemplate(guruConfig);
