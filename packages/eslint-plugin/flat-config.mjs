df48638aebaf8134b8a31e9d682fff636392aa5a
Sean Kinread <dev@kinread.com>
2025-02-20 10:38:09 +1100

    chore: cull the eslint rules to remove rules that should be project-specific

--- packages/eslint-plugin/flat-config.mjs
+++ packages/eslint-plugin/flat-config.mjs
@@ -1,13 +1,15 @@
 import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+
 import reactEslint from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import jsxA11Y from 'eslint-plugin-jsx-a11y';
 import sonarjs from 'eslint-plugin-sonarjs';
-import tsParser from '@typescript-eslint/parser';
+
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -58,14 +60,12 @@ export const imports = [
 			],
 			'import/no-duplicates': 'error',
 
-			'unicorn/import-style': 'off',
-
 			'import/namespace': 'off', // breaks??
 		},
 	},
 ];
 
-// Eslint base config
+// Eslint base config including import
 export const base = [
 	...imports,
 	...fixupConfigRules(
@@ -87,14 +87,6 @@ export const base = [
 		},
 
 		rules: {
-			'func-names': ['warn', 'always'],
-			'no-multi-assign': 'warn',
-			'no-void': 'off',
-			'new-cap': 'off',
-			'no-warning-comments': ['off', { terms: ['TODO'] }],
-			camelcase: 'off',
-			'promise/always-return': 'warn',
-			'promise/catch-or-return': 'warn',
 			'unicorn/filename-case': 'off',
 			'unicorn/prevent-abbreviations': 'off',
 			'unicorn/no-null': 'off',
@@ -107,7 +99,7 @@ export const base = [
 	},
 ];
 
-// Eslint react config
+// Eslint react plugins, settings and rules
 export const react = [
 	...fixupConfigRules(
 		compat.extends(
@@ -127,44 +119,14 @@ export const react = [
 			},
 		},
 		rules: {
-			'react/no-array-index-key': 'off',
-			'react/no-children-prop': 'warn',
-			'react/no-unescaped-entities': 'warn',
+			'react/no-array-index-key': 'warn',
 			'react/prop-types': 'off',
 			'react/display-name': 'off',
-			'react/react-in-jsx-scope': 'off', // React 17 doesnt need this anymore
-
-			// https://github.com/evcohen/eslint-plugin-jsx-a11y/tree/master/docs/rules
-			'jsx-a11y/accessible-emoji': 'warn',
-			'jsx-a11y/alt-text': 'warn',
-			'jsx-a11y/anchor-has-content': 'warn',
-			'jsx-a11y/anchor-is-valid': [
-				'warn',
-				{
-					aspects: ['noHref', 'invalidHref'],
-				},
-			],
-
-			// JSX A11y rules
-			'jsx-a11y/aria-activedescendant-has-tabindex': 'warn',
-			'jsx-a11y/aria-props': 'warn',
-			'jsx-a11y/aria-proptypes': 'warn',
-			'jsx-a11y/aria-role': ['warn', { ignoreNonDOM: true }],
-			'jsx-a11y/aria-unsupported-elements': 'warn',
-			'jsx-a11y/heading-has-content': 'warn',
-			'jsx-a11y/iframe-has-title': 'warn',
-			'jsx-a11y/img-redundant-alt': 'warn',
-			'jsx-a11y/no-access-key': 'warn',
-			'jsx-a11y/no-distracting-elements': 'warn',
-			'jsx-a11y/no-redundant-roles': 'warn',
-			'jsx-a11y/role-has-required-aria-props': 'warn',
-			'jsx-a11y/role-supports-aria-props': 'warn',
-			'jsx-a11y/scope': 'warn',
 		},
 	},
 ];
 
-// TypeScript rules
+// TypeScript plugins, and SonarJS plugin settings and rules
 export const typescript = [
 	...fixupConfigRules(
 		compat.extends(
@@ -194,34 +156,9 @@ export const typescript = [
 			},
 		},
 		rules: {
-			'@typescript-eslint/camelcase': 'off',
-			'@typescript-eslint/explicit-function-return-type': 'off',
-			'@typescript-eslint/generic-type-naming': 'off',
-			'@typescript-eslint/indent': 'off',
-			'@typescript-eslint/no-require-imports': 'off',
-			'@typescript-eslint/ban-types': 'off', // Complains about Omit so cya
-			'sonarjs/no-duplicate-string': 'warn',
-			'@typescript-eslint/no-explicit-any': 'warn',
-			'@typescript-eslint/ban-ts-comment': 'warn',
-			'@typescript-eslint/no-use-before-define': 'warn',
-
-			// SonarJS rules
 			'sonarjs/no-duplicate-string': 'warn',
 			'sonarjs/slow-regex': 'warn',
 			'sonarjs/todo-tag': 'off',
-		},
-	},
-];
-
-export const jest = [
-	...fixupConfigRules(compat.extends('plugin:jest/recommended')),
-	{
-		plugins: {
-			jest: fixupPluginRules(jest),
-		},
-
-		rules: {
-			'jest/prefer-called-with': 'warn',
 		},
 	},
 ];
