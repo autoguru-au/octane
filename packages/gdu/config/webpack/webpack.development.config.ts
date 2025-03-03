14727b2d6b4bb497ae15e92d9f520cfbd9d020da
Amir Zahedi <amir.zahedi@gmail.com>
2025-03-04 08:19:31 +1000

    feat: add crypto-browserify support in webpack configuration (#317)
    * feat: add crypto-browserify support in webpack configuration
    
    * fix: correct html-webpack-plugin name and unify crypto-browserify resolution

--- packages/gdu/config/webpack/webpack.development.config.ts
+++ packages/gdu/config/webpack/webpack.development.config.ts
@@ -114,6 +114,7 @@ export const baseDevelopmentOptions = ({
 				buffer: require.resolve('buffer/'),
 				stream: require.resolve('stream-browserify'),
 				assert: require.resolve('assert/'),
+				crypto: require.resolve('crypto-browserify'),
 			},
 			extensions: ['.tsx', '.ts', '.mjs', '.jsx', '.js', '.json'],
 			plugins: [
