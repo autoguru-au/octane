4f904987282fe56347adb680da5ff94fd1f008a1
Amir Zahedi <amir.zahedi@gmail.com>
2025-08-26 10:18:23 +1000

    feat: Widen supported browser versions in browserslist configuration

--- packages/browserslist-config/index.js
+++ packages/browserslist-config/index.js
@@ -1,16 +1,14 @@
 module.exports = [
-	'last 2 chrome versions',
-
-	'last 2 firefox versions',
+	'last 4 chrome versions',
+	'last 4 firefox versions',
 	'last 4 opera versions',
 	'last 4 edge versions',
-	'last 4 safari versions',
-	'not ios_saf < 11',
-	'last 6 samsung versions',
-
-	'last 2 and_chr versions',
-	'last 2 and_ff versions',
+	'last 10 safari versions',
+	'last 4 samsung versions',
+	'last 4 and_chr versions',
+	'last 4 and_ff versions',
 	'not ie 11',
 	'not op_mini all',
+	'last 1 year',
 	'not dead',
 ];
